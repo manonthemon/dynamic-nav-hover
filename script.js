931b51826509f7db1ef0dbfee91fffb571cbc010
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-26 14:59:25 +0100

    Added showing and dissapearing of content on hover

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
+this.classList.add('trigger-enter')
+setTimeout(() => this.classList.add('trigger-enter-active'), 150)
 
 }
 
 function handleLeave() {
-    
+    this.classList.remove('trigger-enter', 'trigger-enter-active')
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))

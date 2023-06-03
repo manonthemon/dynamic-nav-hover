9fcc08ddefe19dd836819751268bd9be1e008541
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-03 11:34:16 +0100

    Added white background element displaying on page

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const nav = document.querySelector('.top');
 function handleEnter() {
 this.classList.add('trigger-enter')
 setTimeout(() => this.classList.add('trigger-enter-active'), 150)
+background.classList.add('open')
 
 }
 
 function handleLeave() {
     this.classList.remove('trigger-enter', 'trigger-enter-active')
+    background.classList.remove('open')
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))

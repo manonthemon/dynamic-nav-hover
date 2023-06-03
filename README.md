99e4b6608ceb3ba91284e6947f7c2b36117d29ac
Manonthemon <klimowicz.maciej@gmail.com>
2023-06-03 12:02:27 +0100

    Added readme and comments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # dynamic-nav-hover
+
+
+![JS](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E) ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+## Description
+
+This mini projects showcases a dynamically animating dropdown menu backgrounds. 
+
+## Usage
+
+See how it works here: https://manonthemon.github.io/dynamic-nav-hover
+
+## Copyright
+
+This code was written by Maciek Klimowicz following the instructions form the JavaScript30 online course: https://javascript30.com/
--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-const triggers = document.querySelectorAll('.cool > li');
-const background = document.querySelector('.dropdownBackground');
-const nav = document.querySelector('.top');
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
 
 function handleEnter() {
-this.classList.add('trigger-enter')
-setTimeout(() => {
-    if(this.classList.contains('trigger-enter')) {
-    this.classList.add('trigger-enter-active')
+  this.classList.add("trigger-enter");
+  setTimeout(() => {
+    if (this.classList.contains("trigger-enter")) {
+      this.classList.add("trigger-enter-active");
     }
-},10);
-background.classList.add('open')
+  }, 150);
+  background.classList.add("open");
 
-const dropdown = this.querySelector('.dropdown')
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCords = dropdown.getBoundingClientRect(); //Get coordinates of the dropdown
+  const navCords = nav.getBoundingClientRect(); //Gets coordinates of nav in case it's not on top of page
 
-const dropdownCords = dropdown.getBoundingClientRect() //Get coordinates of the dropdown
-const navCords = nav.getBoundingClientRect() //Gets coordinates of nav in case it's not on top of page
-
-const cords = {
+  const cords = {
     height: dropdownCords.height,
     width: dropdownCords.width,
     top: dropdownCords.top - navCords.top,
     left: dropdownCords.left - navCords.left,
+  };
 
-}
-
-background.style.setProperty('width', `${cords.width}px`); //Defines width and height of background of hovered over dropdown
-background.style.setProperty('height', `${cords.height}px`);
-background.style.setProperty('transform', `translate(${cords.left}px, ${cords.top}px )`); // Defines position of the hovered over dropdown
-
-
+  background.style.setProperty("width", `${cords.width}px`); //Defines width and height of background of hovered over dropdown
+  background.style.setProperty("height", `${cords.height}px`);
+  background.style.setProperty(
+    "transform",
+    `translate(${cords.left}px, ${cords.top}px )`
+  ); // Defines position of the hovered over dropdown
 }
 
 function handleLeave() {
-    this.classList.remove('trigger-enter', 'trigger-enter-active')
-    background.classList.remove('open')
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
 }
 
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter))
-triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave))
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
